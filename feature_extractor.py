b7ac035e74311153755d1833ea35c1d23798fa95
Andreas Weise <aweise@gradcenter.cuny.edu>
2017-02-11 09:44:52 -0500

    Add complexity features and fix stemming issue

--- feature_extractor.py
+++ feature_extractor.py
@@ -38,7 +38,7 @@ def preprocess():
         stemmed_text = []
         for t in tokens:
             try:
-                stemmed_text.extend(porter.stem(t))
+                stemmed_text.extend([porter.stem(t)])
             except IndexError:
                 stemmed_text.extend('')
         stemmed_texts.append(stemmed_text)
@@ -107,8 +107,37 @@ def bag_of_ngrams(texts, n=1, m=None):
     return bon
 
 
+def unique_words_ratio():
+    """ returns #unique words / #words for each text
+
+    uses stemmed words so 'eat' and 'eating' etc. are not treated as distinct
+    (assuming they are stemmed correctly; 'eat' and 'ate' are still 'distinct');
+    note that punctuation characters, parentheses etc. are treated as words
+    """
+    return [len(set(text)) / len(text) for text in stemmed_texts]
+
+
+def words_per_sentence():
+    """ returns average number of words per sentence for each text
+
+    uses the '.' POS tag to detect number of sentences to avoid treating '.' in
+    abbreviations as sentence ends
+    """
+    return [word_counts[i] / tagged_texts[i].count('.')
+            for i in range(0, len(word_counts))]
+
+
+def characters_per_words():
+    """ returns average number of characters per word for each text
+
+    note that character count includes punctuation, parentheses etc.
+    """
+    return [(len(source_texts[i]) - word_counts[i] + 1) / word_counts[i]
+            for i in range(0, len(word_counts))]
+
+
 # FILL IN OTHER FEATURE EXTRACTORS
-# TODO complexity and topic models
+# TODO topic models
 
 
 def extract_features(texts, conf):
@@ -133,7 +162,7 @@ def extract_features(texts, conf):
     # each component list will have the same length as the list of input text
     features = []
 
-    # extract requested features: TODO complexity and topic models
+    # extract requested features: TODO topic models
     if 'bag_of_function_words' in conf or all_features:
         features.extend(bag_of_function_words())
     if 'bag_of_pos_trigrams' in conf or all_features:
@@ -148,8 +177,12 @@ def extract_features(texts, conf):
         features.extend(bag_of_ngrams(stemmed_texts, 2, 100))
     if 'bag_of_unigrams' in conf or all_features:
         features.extend(bag_of_ngrams(stemmed_texts, 1, None))
+    if 'characters_per_word' in conf or all_features:
+        features.extend(characters_per_words())
+    if 'unique_words_ratio' in conf or all_features:
+        features.extend(unique_words_ratio())
+    if 'words_per_sentence' in conf or all_features:
+        features.extend(words_per_sentence())
 
     # transpose list of lists so its dimensions are #instances x #features
-    features = numpy.asarray(features).T.tolist()
-
-    return features
+    return numpy.asarray(features).T.tolist()
